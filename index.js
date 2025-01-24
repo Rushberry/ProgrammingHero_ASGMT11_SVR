e160040ce272d420b018270987176adcad7a10de
Rushberry <info.rushberry@gmail.com>
2025-01-24 21:03:31 +0600

    Created an api for getting all the cars added by an user through query

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/myCars', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await carBase.find(query).toArray()
+            res.send(result)
+        })
+
         app.get('/car/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
